1dcb2a45d3e5134218477fcbf668a61e459cb18a
Florian <sicilia.florian02@gmail.com>
2022-04-07 14:33:32 +0200

    Initialisation du projet

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT;
+
+const index = require("./routes/index");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(morgan("short"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(index);
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
